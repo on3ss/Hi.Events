11d7e03d810638979b880041a8dba535076b6254
Oness <auditoree9@gmail.com>
2026-05-20 06:10:00 +0000

    created RazorpayTransfer model

--- backend/app/Models/RazorpayTransfer.php
+++ backend/app/Models/RazorpayTransfer.php
@@ -8,15 +8,22 @@ class RazorpayTransfer extends BaseModel
         'order_id',
         'razorpay_transfer_id',
         'razorpay_payment_id',
+        'razorpay_order_id',
         'linked_account_id',
         'amount',
         'currency',
         'status',
         'raw_payload',
+        'processed_at',
+        'failed_at',
+        'reversed_at',
     ];
 
     protected $casts = [
         'raw_payload' => 'array',
+        'processed_at' => 'datetime',
+        'failed_at' => 'datetime',
+        'reversed_at' => 'datetime',
     ];
 
     public function order()
